a5e08019dc994cab02d58de275bd566b824d1e9e
damien-angioni <damien.angioni@laplateforme.io>
2023-02-02 12:17:10 +0100

    Changement de méthode
    après avoir essayé Pygame, puis Kivy, je préfère utiliser tkinter pour l'interface graphique.

--- Functions.py
+++ Functions.py
@@ -1,16 +1,4 @@
 #Importations des librairies et des annexes.
-import pygame
-import pysound
-from pygame import mixer
+from Player import *
 
-#Définition des images des boutons.
-play_img=pygame.image.load("resources/play.png")
-pause_img=pygame.image.load("resources/pause.png")
-stop_img=pygame.image.load("resources/stop.png")
-list_img=pygame.image.load("resources/list.png")
-previous_img=pygame.image.load("resources/previous.png")
-next_img=pygame.image.load("resources/next.png")
-random_img=pygame.image.load("resources/random.png")
-loop_img=pygame.image.load("resources/loop.png")
-draw_img=pygame.image.load("resources/draw.png")
-delete_img=pygame.image.load("resources/delete.png")
+
--- Player.py
+++ Player.py
@@ -1,28 +1,21 @@
 #Importations des librairies et des annexes.
-import pygame
-import pysound
 from pygame import mixer
-import sys
+from tkinter import *
+import random
+import tkinter.font as font
+from tkinter import filedialog
 from Functions import *
 #définition des variables
-music_list= ["Music/Approaching_Nirvana/Cherry_Blossoms_Approaching_Nirvana.mp3",
-            "Music/Jpop/Kimigamitayumenomonogatari.mp3",
-            "Music/Metal/Wintersun _Beautiful_Death.mp3",
-            "Music/OST/FFBE/Final_Fantasy_Brave_Exvius_Main.mp3",
-            "Music/XC_Engage_the_Enemy.mp3"] #Quelques musiques par défaut, utile pour les tests. (Musiques dans des dossiers différents)
+
 
 def main():
-    #Fonction principale du programmende: lecteur de musique.
-    print("init")
-    screen = pygame.display.set_mode((1080, 720))
-    pygame.display.set_caption("Frestfeld Music Player")
-    pygame.display.set_icon(pygame.image.load("resources/icon.png"))
-    screen.fill((25,25,25))
-    pygame.display.flip()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    #Fonction principale du programmen de: lecteur de musique.
+    fenetre=Tk()
+    fenetre.title("Scarlet Music Player")
+    fenetre.geometry("1080x720")
+    fenetre.iconbitmap("resources/icon.ico")
+    #initialize mixer
+    #mixer.init()
+    fenetre.mainloop()
 
 main() #appel de la fonction principale.
